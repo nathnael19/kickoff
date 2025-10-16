29f670ea705301e8ce8239964a3262b37f9722f4
Nathnael Nigussie <nathnaelnigussie19@gmail.com>
2025-10-16 13:53:58 +0300

    defining the schema for scores

---
+++ app/schemas/scores.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel
+from typing import Optional
+from datetime import datetime
+
+class ScoreBase(BaseModel):
+    match_id: int
+    home_score: Optional[int] = 0
+    away_score: Optional[int] = 0
+    goal_scorers: Optional[list] = None
+    yellow_cards: Optional[list] = None
+    red_cards: Optional[list] = None
+
+class ScoreCreate(ScoreBase):
+    pass
+
+class ScoreResponse(ScoreBase):
+    id: int
+    updated_at: datetime
+
+    class Config:
+        orm_mode = True
