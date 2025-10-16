1e29785a3db39b23a747493c8166661bfca12f5c
Nathnael Nigussie <nathnaelnigussie19@gmail.com>
2025-10-16 13:55:37 +0300

    defining the schema for teams

--- app/schemas/teams.py
+++ app/schemas/teams.py
@@ -1,3 +1,20 @@
 from pydantic import BaseModel
 from typing import Optional, List
 from datetime import datetime
+
+
+class TeamBase(BaseModel):
+    name: str
+    department: str
+    logo_url: Optional[str] = None
+    tournament_id: int
+
+class TeamCreate(TeamBase):
+    pass
+
+class TeamResponse(TeamBase):
+    id: int
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
