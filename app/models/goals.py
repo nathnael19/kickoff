dd5e692faf3d7915e5c11931006fc98be36ac7ee
Nathnael Nigussie <nathnaelnigussie19@gmail.com>
2025-10-16 18:09:38 +0300

    creating a goal record model

---
+++ app/models/goals.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+from sqlalchemy import (
+    Column, Integer, String, Text, ForeignKey,Boolean,
+    TIMESTAMP, JSON, Enum
+)
+from sqlalchemy.orm import relationship, declarative_base
+from sqlalchemy.dialects.postgresql import JSONB
+
+Base = declarative_base()
+
+class GoalRecord(Base):
+    __tablename__ = "goals"
+
+    id = Column(Integer,primary_key=True,index=True)
+    match_id = Column(Integer,ForeignKey("matches.id",ondelete="CASCADE"),unique=True,nullable=False)
+    player_id = Column(Integer,ForeignKey("players.id",ondelete="CASCADE"),unique=True,nullable=False)
+    minute = Column(Integer)
+    is_own_goal = Column(Boolean,default=False)
+    created_at = Column(TIMESTAMP,default=datetime.now)
+
+    # Relationship
+    match = relationship("GoalRecord", back_populates="goals")
+    player = relationship("Players",back_populates="goals")
